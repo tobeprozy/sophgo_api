c4d8a9bafb7741709776edfce3fc848e9fbbcb11
zhiyuan.zhang <zhiyuan.zhang@sophgo.com>
2024-08-05 13:33:30 +0800

    mkdir

---
+++ bmcv/bmcv_basic/main.cpp
@@ -0,0 +1,25 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2023 Sophgo Technologies Inc.  All rights reserved.
+//
+// SOPHON-DEMO is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include <string.h>
+#include <iostream>
+#include "bmcv_api_ext.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  int dev_id = 0;
+  bm_handle_t h;
+  bm_image frame1;
+  bm_image_create(h, 460, 1184, FORMAT_BGR_PACKED,                  DATA_TYPE_EXT_1N_BYTE, &frame1);
+  bm_image frame2;
+  bm_image_create(h, 460, 1184, FORMAT_BGR_PACKED,                  DATA_TYPE_EXT_1N_BYTE, &frame2);
+
+}
