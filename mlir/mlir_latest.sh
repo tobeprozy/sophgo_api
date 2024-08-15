485d53674fb27af88c123c012cc7878539718e6a
zhiyuan.zhang <zhiyuan.zhang@sophgo.com>
2024-08-15 12:01:06 +0800

    add api

---
+++ mlir/mlir_latest.sh
@@ -0,0 +1,16 @@
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/tpu-mlir/daily_build/Master_20240515_053700
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/libsophon/daily_build/Master_20240612_031018/sophon-libsophon_0.5.1_arm64.deb
+
+
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/sophon-img/daily_build/Master_20240614_052015
+
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/tpu-mlir/daily_build/Master_20240614_053700
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/athena2/sophon-img-edge-auto/release_build/20240614_004629
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/athena2/sophon_media/release_build/20240614_002500
+
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/sophon-img/daily_build/Master_20240606_051124
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/libsophon/daily_build/Master_20240611_030951
+
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/sophon-img/daily_build/Master_20240612_051414/sdcard.tgz
+
+wget -nH -m --ftp-user=AI --ftp-password=SophgoRelease2022 ftp://AI@172.28.141.89/sophon-sdk/sophon-img/daily_build/Master_20240616_052120/sdcard.tgz
--- mlir/yolox.sh
+++ mlir/yolox.sh
@@ -15,3 +15,59 @@ model_deploy \
     --test_input bytetrack_s_in_f32.npz \
     --test_reference bytetrack_s_top_outputs.npz \
     --model bytetrack_s_v2_fp32.bmodel
+
+
+
+
+model_transform.py \
+    --model_name yolov10s \
+    --model_def ..//yolov10s.onnx \
+    --input_shapes [[1,3,640,640]] \
+    --mean 0.0,0.0,0.0 \
+    --scale 0.0039216,0.0039216,0.0039216 \
+    --keep_aspect_ratio \
+    --test_input ../bus.jpg \
+    --test_result yolov10s_top_outputs.npz \
+    --pixel_format rgb  \
+    --tolerance 0.99,0.85 \
+    --mlir yolov10s_1b.mlir 
+
+
+run_calibration.py yolov10s_1b.mlir \
+        --dataset ../datasets/coco128/ \
+        --input_num 32 \
+        -o yolov10s_cali_table
+
+
+model_deploy.py \
+        --mlir yolov10s_1b.mlir \
+        --quantize INT8 \
+        --chip bm1684x \
+        --quantize_table ../models/onnx/qtable_y \
+        --calibration_table yolov10s_cali_table \
+        --model yolov10s_int8_1b_y.bmodel
+
+
+
+model_deploy.py \
+    --mlir swim_v10_20240703.mlir \
+    --quantize F32 \
+    --chip bm1684x \
+    --test_input yolov10_in_f32.npz \
+    --test_reference yolov10_outputs.npz \
+    --model swim_v10_20240703_fp32_1b.bmodel
+
+qtable_y
+yolov10s_all_qtable
+model_deploy.py \
+        --mlir yolov10s_1b.mlir \
+        --quantize INT8 \
+        --chip bm1684x \
+        --quantize_table ../models/onnx/qtable_y \
+        --calibration_table yolov10s_cali_table \
+        --test_input yolov10s_in_f32.npz \
+        --test_reference yolov10s_top_outputs.npz \
+        --model yolov10s_int8_1b_y.bmodel
+
+
+fp_forward.py yolov10s_1b.mlir --fpfwd_outputs /model.19/cv2/act/Mul_output_0_Mul --fp_type F32  --processor bm1684x -o yolov10s_qtable1
