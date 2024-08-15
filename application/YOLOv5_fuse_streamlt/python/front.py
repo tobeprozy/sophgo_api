977837cdc54c1f18c997cbae956c62767d2edc36
zhiyuan.zhang <zhiyuan.zhang@sophgo.com>
2024-08-15 10:27:19 +0800

    add api

--- application/YOLOv5_fuse_streamlt/python/front.py
+++ application/YOLOv5_fuse_streamlt/python/front.py
@@ -37,6 +37,7 @@ st.sidebar.title("加载视频或图片")
 files = st.sidebar.file_uploader("上传视频或图片文件", type=["mp4", "avi", "mov", "avi", "jpg", "png"], accept_multiple_files=True, help="支持多张图片上传，如果不上传将使用demo默认图片进行推理")
 url = st.sidebar.text_input("输入视频或图片URL")
 
+print(url)
 
 enter = st.sidebar.button("开始")
 
@@ -44,123 +45,128 @@ enter = st.sidebar.button("开始")
 col1, col2 = st.columns(2)
 
 # 显示实时画面
-col1.title("实时画面")
+col1.title("原始画面")
 video_placeholder = col1.empty()
 
 # 显示后端处理后的图片结果
-col2.title("后端处理结果")
+col2.title("算法结果")
 image_placeholder = col2.empty()
 
 server_url = 'http://localhost:5000'  # 后端服务器URL
 
+
 if enter:
-    for file in files:
-        file_extension = file.name.split(".")[-1].lower()
-        if file_extension in ["jpg", "jpeg", "png"]:
-            frame = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
-            
+    if files:
+        for file in files:
+            file_extension = file.name.split(".")[-1].lower()
+            if file_extension in ["jpg", "jpeg", "png"]:
+                print("jpg,jpeg,png")
+                frame = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
+                
 
-            _, encoded_image = cv2.imencode('.jpg', frame)
-            image_base64 = base64.b64encode(encoded_image).decode('utf-8')
-            id = int(time.time() * 1000) + random.randint(0, 999)
+                _, encoded_image = cv2.imencode('.jpg', frame)
+                image_base64 = base64.b64encode(encoded_image).decode('utf-8')
+                id = int(time.time() * 1000) + random.randint(0, 999)
 
-            print(image_base64)
-            response = requests.post(f'{server_url}/push_data', data={
-                'id': id,
-                'image': image_base64
-            })
+                # print(image_base64)
+                response = requests.post(f'{server_url}/push_data', data={
+                    'id': id,
+                    'image': image_base64
+                })
 
-            print(response.text)
+                # print(response.text)
 
-            if response.status_code == 200:
-                result = response.json()
-                img_name = result.get('frame_id')
-                jpg_base64 = result.get('jpg_base64')
-                if img_name and jpg_base64:
-                    processed_image = base64.b64decode(jpg_base64)
-                    image_placeholder.image(processed_image, caption='Processed Image.', use_column_width=True)
-            else:
-                st.error("后端处理出现问题，请重试或联系管理员")
-            compressed_image1 = compress_image(frame)
-            video_placeholder.image(compressed_image1, caption='origin Image.', channels="BGR")
-        elif file_extension in ["mp4", "avi", "mov"]:
-                # 处理视频
-                # 保存上传的视频文件到本地临时文件夹
-                with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                    temp_file.write(file.read())
-                    video_path = temp_file.name
+                if response.status_code == 200:
+                    result = response.json()
+                    img_name = result.get('frame_id')
+                    jpg_base64 = result.get('jpg_base64')
+                    if img_name and jpg_base64:
+                        processed_image = base64.b64decode(jpg_base64)
+                        image_placeholder.image(processed_image, caption='Processed Image.', use_column_width=True)
+                else:
+                    st.error("后端处理出现问题，请重试或联系管理员")
+                compressed_image1 = compress_image(frame)
+                video_placeholder.image(compressed_image1, caption='origin Image.', channels="BGR")
+            elif file_extension in ["mp4", "avi", "mov"]:
+                    print("mp4,avi,mov")
+                    # 保存上传的视频文件到本地临时文件夹
+                    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                        temp_file.write(file.read())
+                        video_path = temp_file.name
 
-                cap = cv2.VideoCapture(video_path)
-                interval = 10
-                frame_count = 0
-                while cap.isOpened():
-                    ret, frame = cap.read()
-                    if not ret or frame is None:
-                        end_flag = True
-                    if frame_count % interval == 0:
-                        # 在画面上进行后端处理，这里简单地将画面转为灰度图像
-                        _, encoded_image = cv2.imencode('.jpg', frame)
-                        image_base64 = base64.b64encode(encoded_image).decode('utf-8')
-                        id = int(time.time() * 1000) + random.randint(0, 999)
+                    cap = cv2.VideoCapture(video_path)
+                    interval = 10
+                    frame_count = 0
+                    while cap.isOpened():
+                        ret, frame = cap.read()
+                        if not ret or frame is None:
+                            end_flag = True
+                        if frame_count % interval == 0:
+                            # 在画面上进行后端处理，这里简单地将画面转为灰度图像
+                            _, encoded_image = cv2.imencode('.jpg', frame)
+                            image_base64 = base64.b64encode(encoded_image).decode('utf-8')
+                            id = int(time.time() * 1000) + random.randint(0, 999)
 
-                        print(image_base64)
-                        response = requests.post(f'{server_url}/push_data', data={
-                            'id': id,
-                            'image': image_base64
-                        })
+                            # print(image_base64)
+                            response = requests.post(f'{server_url}/push_data', data={
+                                'id': id,
+                                'image': image_base64
+                            })
 
-                        print(response.text)
+                            # print(response.text)
 
-                        if response.status_code == 200:
-                            result = response.json()
-                            img_name = result.get('frame_id')
-                            jpg_base64 = result.get('jpg_base64')
-                            if img_name and jpg_base64:
-                                processed_image = base64.b64decode(jpg_base64)
-                                image_placeholder.image(processed_image, caption='Processed Image.', use_column_width=True)
-                        else:
-                            st.error("后端处理出现问题，请重试或联系管理员")
-                        compressed_image1 = compress_image(frame)
-                        video_placeholder.image(compressed_image1, caption='origin Image.', channels="BGR")
+                            if response.status_code == 200:
+                                result = response.json()
+                                img_name = result.get('frame_id')
+                                jpg_base64 = result.get('jpg_base64')
+                                if img_name and jpg_base64:
+                                    processed_image = base64.b64decode(jpg_base64)
+                                    image_placeholder.image(processed_image, caption='Processed Image.', use_column_width=True)
+                            else:
+                                st.error("后端处理出现问题，请重试或联系管理员")
+                            compressed_image1 = compress_image(frame)
+                            video_placeholder.image(compressed_image1, caption='origin Image.', channels="BGR")
 
-                    frame_count += 1
-        elif url:
-            # 处理URL
-                cap = cv2.VideoCapture(url)
-                interval = 10
-                frame_count = 0
-                while cap.isOpened():
-                    ret, frame = cap.read()
-                    if not ret or frame is None:
-                        end_flag = True
-                    if frame_count % interval == 0:
-                        # 在画面上进行后端处理，这里简单地将画面转为灰度图像
-                        _, encoded_image = cv2.imencode('.jpg', frame)
-                        image_base64 = base64.b64encode(encoded_image).decode('utf-8')
-                        id = int(time.time() * 1000) + random.randint(0, 999)
+                        frame_count += 1
+    elif url:
+        # 处理URL
+        print("url")
+        cap = cv2.VideoCapture(url)
+        interval = 10
+        frame_count = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret or frame is None:
+                end_flag = True
+            if frame_count % interval == 0:
+                # 在画面上进行后端处理，这里简单地将画面转为灰度图像
+                _, encoded_image = cv2.imencode('.jpg', frame)
+                image_base64 = base64.b64encode(encoded_image).decode('utf-8')
+                id = int(time.time() * 1000) + random.randint(0, 999)
 
-                        print(image_base64)
-                        response = requests.post(f'{server_url}/push_data', data={
-                            'id': id,
-                            'image': image_base64
-                        })
+                # print(image_base64)
+                response = requests.post(f'{server_url}/push_data', data={
+                    'id': id,
+                    'image': image_base64
+                })
 
-                        print(response.text)
+                # print(response.text)
 
-                        if response.status_code == 200:
-                            result = response.json()
-                            img_name = result.get('frame_id')
-                            jpg_base64 = result.get('jpg_base64')
-                            if img_name and jpg_base64:
-                                processed_image = base64.b64decode(jpg_base64)
-                                image_placeholder.image(processed_image, caption='Processed Image.', use_column_width=True)
-                        else:
-                            st.error("后端处理出现问题，请重试或联系管理员")
-                        compressed_image1 = compress_image(frame)
-                        video_placeholder.image(compressed_image1, caption='origin Image.', channels="BGR")      
-                    frame_count += 1
-        else:
-            st.warning("请上传视频或图片文件或输入视频或图片URL")
+                if response.status_code == 200:
+                    result = response.json()
+                    img_name = result.get('frame_id')
+                    jpg_base64 = result.get('jpg_base64')
+                    if img_name and jpg_base64:
+                        processed_image = base64.b64decode(jpg_base64)
+                        image_placeholder.image(processed_image, caption='Processed Image.', use_column_width=True)
+                        print("success")
+                else:
+                    st.error("后端处理出现问题，请重试或联系管理员")
+                compressed_image1 = compress_image(frame)
+                video_placeholder.image(compressed_image1, caption='origin Image.', channels="BGR")      
+            frame_count += 1
+    else:
+        st.warning("请上传视频或图片文件或输入视频或图片URL")
 
 
 
